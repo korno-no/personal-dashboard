065dcae9dbda54048bf47a523477f904e741cce4
Korno <korno9612@gmail.com>
2025-09-19 23:28:44 +0300

    add option Done for task

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -1,56 +1,90 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {appAutoFocus} from '../../shared/directives/auto-focus'
 import { Button } from "../../shared/ui/button/button";
 import { InputComponent } from "../../shared/ui/input/input";
+
+interface Task{
+  id: number;
+  text: WritableSignal<string>;
+  editText: WritableSignal<string>;
+  editing: WritableSignal<boolean> ;
+  isDone: WritableSignal<boolean>;
+}
 
 
 @Component({
   selector: 'app-tasks',
-  imports: [FormsModule, appAutoFocus, Button, InputComponent],
+  imports: [FormsModule, Button, InputComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-
-  tasks = <{
-    text: WritableSignal<string>;
-    editText: WritableSignal<string>;
-    editing: WritableSignal<boolean> ;
-  }[]>([]);
   
-  newTask = signal<string>('')
+  private nextId = 0; 
+
+  tasks = signal<Task[]>([]);
+  newTask = signal<string>('');
+  
+
+  sortedTasks = computed(() =>
+  this.tasks().slice().sort((a, b) => Number(a.isDone()) - Number(b.isDone()))
+  );
 
   onAddNewTask(){
     const newTaskText = this.newTask()
     if(!newTaskText?.trim()) return;
 
     const newTaskObj = {
+      id: this.nextId++,
       text: signal(newTaskText),
       editText: signal(newTaskText),
-      editing: signal(false)
+      editing: signal(false),
+      isDone: signal(false),
     }
-
-    this.tasks.unshift(newTaskObj)
+    this.tasks.update(tasks => [...tasks, newTaskObj])
     this.newTask.set('');
   }
 
   onStartEditTask(index: number){
-    this.tasks[index].editing.set(true)
+    this.tasks.update(tasks => 
+      tasks.map(task => task.id === index 
+      ? (()=>{
+        task.editing.set(true)
+        return task
+      })() 
+      : task)
+    );
+
   }
 
   onCancelEditTask(index: number){
-    this.tasks[index].editing.set(false);
-    this.tasks[index].editText.set(this.tasks[index].text());
+    this.tasks.update( tasks =>
+      tasks.map(task => task.id === index 
+      ? (() => {
+        task.editText.set(task.text());
+        task.editing.set(false);
+        return task;
+      })() 
+      : task)
+    )
   }
 
   onSaveEditTask(index: number){
-    if(!this.tasks[index].text().trim()) return
-    this.tasks[index].text.set( this.tasks[index].editText())
-    this.tasks[index].editing.set(false)
+    if(!this.tasks()[index].text().trim()) return
+    this.tasks.update( tasks =>
+       tasks.map(task => task.id === index 
+        ? (()=>{
+          task.text.set(task.editText());
+          task.editing.set(false);
+          return task
+        })() 
+        : task)
+    )
   }
 
   onDeleteTask(index: number){
-    this.tasks.splice(index,1)
+    this.tasks.update(tasks => 
+      tasks.filter(task => task.id !== index)
+    )
   }
 }
