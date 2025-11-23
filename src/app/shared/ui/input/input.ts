ff336ea932f51a55688d7a01558d6937e2e2d635
Korno <korno9612@gmail.com>
2025-11-23 19:18:53 +0200

    add to app-input ControlValueAccessor

--- src/app/shared/ui/input/input.ts
+++ src/app/shared/ui/input/input.ts
@@ -1,31 +1,82 @@
-import { Component, EventEmitter, Input, Output, Signal } from '@angular/core';
+import {
+  Component,
+  Input,
+  forwardRef
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
   templateUrl: './input.html',
-  styleUrl: './input.css'
+  styleUrl: './input.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
 
   @Input() placeholder = '';
   @Input() id = '';
   @Input() label = '';
-  @Input() type = '';
-  @Input() value: string = '';
-  @Input() checked?: boolean;
+  @Input() type = ''; // text | textarea | checkbox
   @Input() name = '';
-  @Output() valueChange = new EventEmitter<string>()
-  @Output() checkedChange = new EventEmitter<boolean>()
 
+  value: any = '';
+  checked: boolean = false;
+
+  disabled = false;
+
+  // callbacks
+  private onChange = (value: any) => {};
+  private _onTouched = () => {};
+
+  onTouched() {
+    this._onTouched(); 
+  }
+
+  // Angular writes value → UI
+  writeValue(value: any): void {
+    if (this.type === 'checkbox') {
+      this.checked = !!value;
+    } else {
+      this.value = value ?? '';
+    }
+  }
+
+  // Angular registers change fn
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  // Angular registers touched fn
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  // disabled state
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  // UI → Angular
   onInput(event: Event) {
-    
     const target = event.target as HTMLInputElement;
 
     if (this.type === 'checkbox') {
-    this.checkedChange.emit(target.checked); // boolean
-    } else{
-      this.valueChange.emit(target.value);
+      this.checked = target.checked;
+      this.onChange(this.checked);
+    } else {
+      this.value = target.value;
+      this.onChange(this.value);
     }
+
+    this.onTouched();
   }
 }
